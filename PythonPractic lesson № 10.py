7739a50234df2437f4c32e0330ad3ca9531d8dd8
alexandralareshina <131816922+alexandralareshina@users.noreply.github.com>
2023-12-22 11:44:45 +0400

    Add files via upload

---
+++ PythonPractic lesson № 10.py
@@ -0,0 +1,44 @@
+import sys
+pets = dict()
+n = 100  # количество операций
+for i in range(n):
+    print('Введите команду: add - добавить питомца, info - просмотр питомцев, exit - завершить : ')
+    zapros = input()
+    if zapros == 'add':
+        # ввод нового питомца
+        name = input('Имя питомца: ')
+        vid = input('Вид питомца: ')
+        age = int(input('Возраст питомца: '))
+        vlad = input('Имя владельца: ')
+        # заполнение словаря
+        pets[name] = {'vid': vid, 'age': age, 'vlad': vlad}
+    elif zapros == 'info':
+        # просмотр информации
+        if len(pets) == 0:
+            print('Нет питомцев!')
+        else:
+            inf = list(pets.keys())
+            print(*inf,sep=', ')
+            print('Введите имя питомца для просмотра информации: ')
+            kl = input()
+            if kl in pets:
+                # присвоение значений словаря переменным для вывода
+                m = list(pets.get(kl).values())
+                n = kl
+                v = m[0]
+                a = m[1]
+                vl = m[2]
+                # адаптация год, года или лет для возраста
+                if (a % 10) == 1 :
+                    ag = str(a) + ' год'
+                elif (a % 10) > 1 and a % 10 < 5 :
+                    ag = str(a) + ' года'
+                else:
+                    ag = str(a) + ' лет'
+                print(f'Это {v} по кличке {n}. Возраст питомца: {ag} . Имя владельца: {vl}.')
+            else:
+                print('Такого питомца нет в базе. Введите add для добавления!')
+    elif zapros == 'exit':
+        sys.exit('Завершение программы')
+    else:
+        print('Неверная команда! Введите add, info или exit!')
